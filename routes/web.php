16b377841d63ffa50b487bc64f91ce0f2369eb2b
TruongDucAnh1117 <ducanh.devcode2021@gmail.com>
2022-05-26 16:10:21 +0700

    Sua link login

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,10 @@ use App\Http\Controllers\Controller\HomeController;
 
 Route::get('/', function () {
     return view('index');
-});
+})->name('index');
 
 
 Route::get('/login', function () {
     return view('login');
-});
+})->name('login');
 
