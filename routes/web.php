485c0bae638e32cdb19b390f71a1297bece66136
TruongDucAnh1117 <ducanh.devcode2021@gmail.com>
2022-05-26 15:36:58 +0700

    Tach main header voi footer

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\Controller\HomeController;
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
-Auth::routes();
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/login', function () {
+    return view('login');
+});
+
